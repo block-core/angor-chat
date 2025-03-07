f56b72712bb22ec3e27526e8a6e8aca6710e750b
Milad Raeisi <miladsoft@yahoo.com>
2025-03-07 19:02:05 +0400

    Remove unused imports from login component

--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -1,17 +1,13 @@
 import {RouteComponentProps, useNavigate} from '@reach/router';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import {useTheme} from '@mui/material/styles';
 import {Helmet} from 'react-helmet';
 
 import Login from 'views/components/login';
 import useMediaBreakPoint from 'hooks/use-media-break-point';
 import useTranslation from 'hooks/use-translation';
-import Github from 'svg/github';
-
 
 const LoginPage = (_: RouteComponentProps) => {
-    const theme = useTheme();
     const {isSm} = useMediaBreakPoint();
     const [t,] = useTranslation();
     const navigate = useNavigate();
