3339d1616a7ca7e466f790b0fbde19be90d996dd
Milad Raeisi <miladsoft@yahoo.com>
2025-04-01 09:48:03 +0400

    Update angor chat

--- src/const.ts
+++ src/const.ts
@@ -17,11 +17,11 @@ export const ACCEPTABLE_LESS_PAGE_MESSAGES = 5;
 export const SCROLL_DETECT_THRESHOLD = 5;
 
 export const GLOBAL_CHAT: Channel = {
-    id: 'f412192fdc846952c75058e911d37a7392aa7fd2e727330f4344badc92fb8a22',
-    name: 'Global Chat',
+    id: 'aa752aef9bc47b8cfb765b6ae08f83745dac563bf564f9c8defffc680cad1300',
+    name: 'Angor Chat',
     about: 'Whatever you want it to be, just be nice',
-    picture: '',
-    creator: 'aea59833635dd0868bc7cf923926e51df936405d8e6a753b78038981c75c4a74',
+    picture: 'https://angor.io/angor-logo.jpg',
+    creator: '5f432a9f39b58ff132fc0a4c8af10d42efd917d8076f68bb7f2f91ed7d4f6a41',
     created: 1678198928
 };
 
